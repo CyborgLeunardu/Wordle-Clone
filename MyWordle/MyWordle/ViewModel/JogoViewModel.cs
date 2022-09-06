9d079bd5b7760ab49726317c0f60002f9eb92766
Leonardo Castilho <leonardo.castilho@viannasempre.com.br>
2022-09-06 10:11:49 -0300

    ViewModel
    Feito a base inicial da ViewModel

--- MyWordle/MyWordle/ViewModel/JogoViewModel.cs
+++ MyWordle/MyWordle/ViewModel/JogoViewModel.cs
@@ -1,9 +1,46 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MyWordle.Model;
 
 namespace MyWordle.ViewModel
 {
     public partial class JogoViewModel : ObservableObject
     {
+        private int linhaIndex;
+        private int colunaIndex;
 
+        [ObservableProperty]
+        private LinhaDaPalavra[] linhas;
+
+        [RelayCommand]
+        public void Entrar()
+        {
+            if (colunaIndex != 5)
+                return;
+
+            var ehValido = true;
+
+            if (ehValido)
+            {
+                if (linhaIndex == 5)
+                {
+                    //TERMINAR JOGO
+                }
+                else
+                {
+                    linhaIndex++;
+                    colunaIndex = 0;
+                }
+            }
+            linhaIndex = 0;
+            colunaIndex = 0;
+        }
+
+        [RelayCommand]
+        public void EntrarLetra(char Letra)
+        {
+            if (colunaIndex == 5)
+                return;
+        }
     }
 }
